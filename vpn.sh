04a9ef4417b1c1a7bb0578dd37f65f967a59fd1a
Catalina <cvajiac@nd.edu>
2018-04-04 20:16:38 -0400

    dont manually specify pid file

--- vpn.sh
+++ vpn.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-PIDDIR="/var/run/vpnc/"
-PIDFILE=$PIDDIR"pid"
-
 usage() {
   cat << EOF
 usage: `basename "$0"` [options]
@@ -16,20 +13,12 @@ if [ $# -eq 0 ]; then
   usage 1
 fi
 
-if [ ! -d $PIDDIR ]; then
-  mkdir -p $PIDDIR
-fi
-
-if [ ! -f $PIDFILE ]; then
-  touch $PIDFILE
-fi
-
 while [ $# -gt 0 ]; do
   if [ "$1" = '-h' ]; then
     usage 0
   elif [ "$1" = '-c' ]; then
     echo "Connecting..."
-    sudo openconnect -q -u cvajiac --pid-file=/var/run/vpnc/pid -b vpnaccess.nd.edu
+    sudo openconnect -q -u cvajiac -b vpnaccess.nd.edu
   elif [ "$1" = '-d' ]; then
     echo "Disconnecting..."
     sudo pkill openconnect
