80d34762cc4da3f3ced7b7a4ae6bc7b38ab93334
Catalina <cvajiac@nd.edu>
2018-10-05 20:12:56 -0400

    add all

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 DIR=`pwd`
 NOTIFYD=".config/notifyd/scripts"
-BIN=".local/bin"
+BIN="$HOME/.local/bin"
+
+link_script() {
+  if [ -f $2 ] || [ -d $2 ]; then
+    rm $2
+  fi
+  echo "Linking $2..."
+  ln -s $1 $2
+}
 
 declare -A FILES=(
-  [anyconnect]=".anyconnect"
-  [bashrc]=".bashrc"
   [i3conf]=".config/i3/config"
-  [i3statusconf]=".i3status.conf"
-  [le_petit_prince.sh]="$BIN/le_petit_prince"
-  [lfptrc]=".lfptrc"
-  [pdf.sh]="$BIN/pdf"
   [polybar]=".config/polybar"
-  [signal.sh]="$BIN/signal"
   [ssh_config]=".ssh/config"
   [term_conf]=".config/terminator/config"
-  [tmux_conf]=".tmux.conf"
   [user-dirs]=".config/user-dirs.dirs"
-  [vim]=".vim"
-  [vimrc]=".vimrc"
-  [vpn.sh]="$BIN/vpn"
 )
+
+
+SCRIPTS=`ls scripts/`
+for SCRIPT in $SCRIPTS; do
+  link_script "$DIR/scripts/$SCRIPT" "$BIN/$SCRIPT"
+done
+
+for FILE in `ls rc_files`; do
+  link_script "$DIR/rc_files/$FILE" "$HOME/$FILE"
+done
 
 for FILE in "${!FILES[@]}"; do
   ABS_PATH=$HOME/${FILES[$FILE]} 
-  if [ -f $ABS_PATH ] || [ -d $ABS_PATH ]; then
-    echo "Removing old $FILE..."
-    rm -rf $ABS_PATH
-  fi
-  echo "Linking      $FILE..."
-  ln -s $DIR/$FILE $ABS_PATH
+  link_script "$DIR/$FILE" "$ABS_PATH"
 done
 
-ln -s "$HOME/$NOTIFYD/notifyd-toggle.sh" "$HOME/$BIN/toggle-notifications"
+#link_script "$HOME/$NOTIFYD/notifyd-toggle.sh" "$HOME/$BIN/toggle-notifications"
